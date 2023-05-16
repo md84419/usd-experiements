ccbb2f1fc7210dea0e9407199e18313290a46c4b
Michael Davey <michael.davey@robotica.ml>
2023-05-16 22:01:40 +0100

    Creating a custom reference

--- exts/robotica.usd.experiments/robotica/usd/experiments/extension.py
+++ exts/robotica.usd.experiments/robotica/usd/experiments/extension.py
@@ -1,6 +1,14 @@
+import os
+from pathlib import Path
+
 import omni.ext
 import omni.ui as ui
+from pxr import UsdShade, Tf, Sdf, Usd
 
+CURRENT_PATH = Path(__file__).parent
+PROJECT_ROOT = CURRENT_PATH.parent.parent.parent
+DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
+TEST_USDA = os.path.join(DATA_DIR, 'usd-experiements.usda')
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
 def some_public_function(x: int):
@@ -12,6 +20,9 @@ def some_public_function(x: int):
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
 # on_shutdown() is called.
 class RoboticaUsdExperimentsExtension(omni.ext.IExt):
+    def __init__(self):
+        self._prim_path = None
+
     # ext_id is current extension id. It can be used with extension manager to query additional information, like where
     # this extension is located on filesystem.
     def on_startup(self, ext_id):
@@ -29,6 +40,63 @@ class RoboticaUsdExperimentsExtension(omni.ext.IExt):
                     self._count += 1
                     label.text = f"count: {self._count}"
 
+                    method1()
+                    method2()
+
+                
+                def method1():
+                    # The following is based on code by Michal S in NVIDIA Omniverse Discord:
+                    # https://discord.com/channels/827959428476174346/989589513909203014/1108002804695572480
+
+                    reference_path = TEST_USDA
+                    name = os.path.splitext(os.path.basename(reference_path))[0]
+                    stage = omni.usd.get_context().get_stage()
+
+                    if self._prim_path is None:
+                        if stage.HasDefaultPrim():
+                            self._prim_path = omni.usd.get_stage_next_free_path(
+                                stage, stage.GetDefaultPrim().GetPath().pathString + "/" + Tf.MakeValidIdentifier(name), False
+                            )
+                        else:
+                            self._prim_path = omni.usd.get_stage_next_free_path(stage, "/" + Tf.MakeValidIdentifier(name), False)
+                        omni.kit.commands.execute("CreateReference", usd_context=omni.usd.get_context(), path_to=self._prim_path, asset_path=reference_path, instanceable=False)
+
+                    scale = 2.5
+                    #some of my attempts
+                    if scale is not None and scale != 1:
+                        scale = float(scale)
+                        UsdShade.Material(stage.GetPrimAtPath(self._prim_path)).CreateInput("texture_scale", Sdf.ValueTypeNames.Float2).Set((scale, scale))
+                        for prim in Usd.PrimRange(stage.GetPrimAtPath(self._prim_path)):
+                            if prim.IsA(UsdShade.Shader):
+                                prim = UsdShade.Shader(prim)
+                                inp = prim.GetInput("scale")
+                                if inp:
+                                    inp.Set(tuple(scale * value for value in inp.Get()))
+                                else:
+                                    prim.CreateInput("scale", Sdf.ValueTypeNames.Float2).Set((scale, scale))
+                    return self._prim_path
+
+                def method2():
+                    '''
+                    Read from the 'global' section of the usda:
+
+                    ```
+                    #usda 1.0
+                    (
+                        customLayerData = {
+                        ...
+                        }
+                        endTimeCode = 100
+                        metersPerUnit = 0.01
+                        startTimeCode = 0
+                        timeCodesPerSecond = 24
+                        upAxis = "Y"
+                    )
+                    ```
+                    '''
+                    return self._prim_path
+                
+
                 def on_reset():
                     self._count = 0
                     label.text = "empty"
@@ -36,8 +104,9 @@ class RoboticaUsdExperimentsExtension(omni.ext.IExt):
                 on_reset()
 
                 with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+                    ui.Button("Experiment", clicked_fn=on_click)
+        
 
     def on_shutdown(self):
         print("[robotica.usd.experiments] robotica usd experiments shutdown")
+        self._window = None
